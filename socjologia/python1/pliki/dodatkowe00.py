54353a1731a369a759501c91630dcc7c02bcb34c
szymongorka <szymgo@gmail.com>
2019-10-23 15:19:38 +0200

    Add fodlers

---
+++ socjologia/python1/pliki/dodatkowe00.py
@@ -0,0 +1,34 @@
+print('Hello')
+# uzykaj imię użykwonika
+
+# sprawdz czy wprowadzone imię składa się z samych małych liter od malej litery
+# (metoda islower - zwraca nam true/false wynik ten jest potrzebny w następnym punkcie)
+
+# jeśli tak zamień pierwszą literę na dużą (np metoda capitalize)
+# if tutaj warunek:
+#	napis = napis.capitlize()
+
+
+# powitaj go "It is good to meet you, name!"
+
+# Oblicz długość jego imienia i wypisz ją
+
+print("The length of your name is:")
+
+# Zmień litery w imieniu użytkonwika na duże litery i wyświetl
+
+# spytaj użykwonika o drugie imię
+
+# stwórz zmienną, która będzie zawietać oba imiona np. "JOHN Mike"
+
+# Zmień powyższy napis tak, aby tylko pierwsze litery imion były duże (metoda title)
+
+
+# spytaj użytkownika o wiek, podaj mu rok w którym osiągnie 100 lat
+
+
+#=======================================
+# drugi program
+# Oblicznaie pola prostąkota
+# poproś użytkownika, aby Ci podał długość i szerokość prostokąta (uwzględnij, że może to być np 2.5)
+# Następnie oblicz pole prostąkota
